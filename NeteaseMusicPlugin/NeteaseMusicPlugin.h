7729e2c47f1d03e0f0f13385c8ef42eaa27da0bf
Jesse Zhu <jesse@itjesse.cn>
2017-05-04 05:44:57 +0900

    Initial Commit

---
+++ NeteaseMusicPlugin/NeteaseMusicPlugin.h
@@ -0,0 +1,19 @@
+//
+//  NeteaseMusicPlugin.h
+//  NeteaseMusicPlugin
+//
+//  Created by Jesse Zhu on 2017/5/4.
+//  Copyright © 2017年 Jesse Zhu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NeteaseMusicPlugin.
+FOUNDATION_EXPORT double NeteaseMusicPluginVersionNumber;
+
+//! Project version string for NeteaseMusicPlugin.
+FOUNDATION_EXPORT const unsigned char NeteaseMusicPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeteaseMusicPlugin/PublicHeader.h>
+
+
